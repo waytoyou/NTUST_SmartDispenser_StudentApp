f13333c1b941f077d0cdf86c200feafccbb1219f
thariqramadhan <athariqramadhan@yahoo.com>
2019-07-12 17:45:44 +0800

    Merge pull request #8 from ianjoseph2204/Project-Thariq
    report-problem-page, maintenance-records-page ver 2019.07.12

--- src/app/pages/maintenance-records/maintenance-records.page.ts
+++ src/app/pages/maintenance-records/maintenance-records.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { DispenserAPIService } from 'src/app/services/DispenserAPI/dispenser-api.service';
 
 @Component({
   selector: 'app-maintenance-records',
@@ -9,22 +10,35 @@ import { Router } from '@angular/router';
 })
 export class MaintenanceRecordsPage implements OnInit {
 
-  constructor(public http: HttpClient, private router: Router) {
+  // Initiate data
+  maintenanceData: any;
+  selectedDeviceId: string = "";
+  backgroundImg: any;
+
+  constructor(public http: HttpClient, private router: Router, private api: DispenserAPIService) {
     this.getAPI();
   }
-    
-  maintenanceData: any;
 
   ngOnInit() {
-   
+
   }
 
+  /*
+  This method is to get data from API and change the format to 
+  {
+    year: 2019,
+    monthMaintenance: {
+                        Month: January,
+                        dayMaintenance: [Data From API] 
+    }   
+  }
+  */
   async getAPI() {
+    await this.prefDeviceId();
+    this.backgroundImg = await this.getPicture(this.selectedDeviceId);
     const apiUrl = 'https://smartcampus.et.ntust.edu.tw:5425/Dispenser/Maintenance?Device_ID=T4_04_01';
     let getAPI = await this.http.get(apiUrl).toPromise();
-
     let errorMeaning = ["Button does not respond", "Unable to water", "Leaking water", "Screen not shown", "Other"];
-
     let dayArray = [];
     for (let i = getAPI['Data'].length - 1; i >= 0; i--) {
       let dataForMaintenance = {
@@ -39,12 +53,14 @@ export class MaintenanceRecordsPage implements OnInit {
       };
       dayArray.push(dataForMaintenance);
     }
+
     let dayMaintenance = [];
     let monthArray = [];
     let monthMaintenance = [];
     let yearArray = [];
     let lastMonth;
     let lastYear;
+
     for (let i = 0; i < dayArray.length; i++) {
       if (i == 0) {
         lastMonth = dayArray[i]['Month'];
@@ -72,7 +88,7 @@ export class MaintenanceRecordsPage implements OnInit {
             monthMaintenance.push(monthArray[i]);
           }
         }
-
+        // If last array
         if (i == dayArray.length - 1) {
           monthArray.push({
             'month': lastMonth,
@@ -86,22 +102,15 @@ export class MaintenanceRecordsPage implements OnInit {
       }
     }
     this.maintenanceData = yearArray;
-    // this.maintenanceData = [
-    //   {
-    //     'month': "March",
-    //     'DayMaintenacne': [
-    //       {
-    //         'ErrorMeaning': "Other"
-    //       }
-    //     ]
-    //   }
-    // ];
-    console.log(this.maintenanceData);
 
-    // console.log(this.maintenanceData);
+    //console.log(this.maintenanceData);
   }
 
 
+
+  /*
+  This method is to reconstruct date format from API
+  */
   getTime(time) {
     // time passed is String, construct into Date format
     // time example from json: "2019-03-08 16:32:00"
@@ -121,24 +130,6 @@ export class MaintenanceRecordsPage implements OnInit {
     let resultMonth = splitDate[1] - 1;
     let resultDateOfMonth = splitDate[2];
 
-    // // split HOUR into HOUR, MINUTE, and SECOND
-    // let splitHour = resultHour.split(":");
-
-    // let resultHourC = splitHour[0];
-    // let resultMinute = splitHour[1];
-    // let resultSecond = splitHour[2];
-
-    // now we get every component to construct date from String
-    // let newDate = new Date(
-    //   resultYear,
-    //   resultMonth,
-    //   resultDateOfMonth,
-    //   resultHourC,
-    //   resultMinute,
-    //   resultSecond,
-    //   0
-    // );
-
     let newDate = {
       'dayForTime': resultDateOfMonth,
       'monthForTime': monthName[resultMonth],
@@ -151,7 +142,25 @@ export class MaintenanceRecordsPage implements OnInit {
   /**
    * Methods for routing to another page
    */
-  goToDashboard(){
+  goToDashboard() {
     this.router.navigate(['dashboard']);
   }
+
+  /*
+  Method to get device ID
+  */
+  async prefDeviceId() {
+    //await this.pref.getData(StaticVariable.KEY__NEARBY_DISPENSER__DEVICE_ID).then((value) => {
+    this.selectedDeviceId = 'EE_06_01';
+    //});
+  }
+
+  /*
+  Method to get picture of device
+  */
+  async getPicture(device_id) {
+    let myUrl = await this.api.getDispenserPictureUrlOnly(device_id);
+    return myUrl;
+  }
+
 }
