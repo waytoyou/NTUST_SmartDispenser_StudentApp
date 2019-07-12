5fd6e1641f5081804c496dd52147e26c75490bfd
Aldy <muhamadaldy17@gmail.com>
2019-07-12 17:51:00 +0800

    dispenser-api.service ver 2019.07.10

--- src/app/services/DispenserAPI/dispenser-api.service.ts
+++ src/app/services/DispenserAPI/dispenser-api.service.ts
@@ -110,14 +110,8 @@ export class DispenserAPIService {
         console.error("Function error: on registerNewUser => " + e);
         return false;
       });
-    
-      // return await this.http.post(url, postDataRegister, httpOption).subscribe(() => {
-      //   return true;
-      // }, () => {
-      //   console.error("Promise rejected: unable to get token!");
-      // });
     }
-
+    
   }
 
   /**
@@ -157,12 +151,6 @@ export class DispenserAPIService {
         console.error("Function error: on loginUser => " + e);
         return false;
       });
-
-    // if (value['code'] === 200) {
-    //   respond = true;
-    // }
-    
-    // return respond;
   }
 
   /**
@@ -246,14 +234,6 @@ export class DispenserAPIService {
         console.error("Function error: on getNearbyDispenser => " + e);
         return [{}];
       });
-    
-    // try {
-    //   let value = await this.http.get(url).toPromise();
-    //   return value['Data'];
-    // } catch (e) {
-    //   console.error("Error in getNearbyDispenser: " + e);
-    //   return [{}];
-    // }
   }
 
   /**
@@ -280,14 +260,6 @@ export class DispenserAPIService {
         console.error("Function error: on getDispenserPicture => " + e);
         return null;
       });
-    
-    // try {
-    //   let value = await this.http.get(url).toPromise();
-    //   return value['Data'];
-    // } catch (e) {
-    //   console.error("Error in getDispenserPicture: " + e);
-    //   return null;
-    // }
   }
 
   /**
@@ -346,19 +318,6 @@ export class DispenserAPIService {
       });
 
     return returnValue;
-    
-    // try {
-    //   let value = await this.http.get(url).toPromise();
-    //   return value['Data'];
-    // } catch (e) {
-    //   console.error("Error in getDispenserDetail: " + e);
-    //   return {
-    //     "Device_ID": device_id,
-    //     "Building": "",
-    //     "Position": "",
-    //     "Type": ""
-    //   };
-    // }
   }
 
   /**
@@ -404,14 +363,6 @@ export class DispenserAPIService {
         console.error("Function error: on getDispenserMaintenance => " + e);
         return [{}];
       });
-    
-    // try {
-    //   let value = await this.http.get(url).toPromise();
-    //   return value['Data'];
-    // } catch (e) {
-    //   console.error("Error in getDispenserMaintenance: " + e);
-    //   return [{}];
-    // }
   }
 
   /**
@@ -458,24 +409,6 @@ export class DispenserAPIService {
       });
 
     return returnValue;
-
-    // let myJson;
-    // await this.http.get(url).toPromise().then((success) => {
-    //   console.log("Success");
-    //   myJson = success['Data'];
-    // }).catch(() => {
-    //   console.error("Error while getting data: " + device_id + ", in getDispenserRawData");
-    //   myJson = {
-    //     "Device_ID": device_id,
-    //     "UploadTime": "",
-    //     "Status": -1,
-    //     "HotTemp": -1,
-    //     "WarmTemp": -1,
-    //     "ColdTemp": -1
-    //   }
-    // });
-
-    // return myJson;
   }
 
   /**
@@ -522,20 +455,6 @@ export class DispenserAPIService {
       });
 
     return returnValue;
-    
-    // try {
-    //   let value = await this.http.get(url).toPromise();
-    //   return value['Data'];
-    // } catch (e) {
-    //   console.error("Error in getDispenserRepairCondition: " + e);
-    //   return {
-    //     "Device_ID": device_id,
-    //     "UploadTime": "",
-    //     "Status": -1,
-    //     "ErrorType": -1,
-    //     "Description": ""
-    //   };
-    // }
   }
 
   /**
