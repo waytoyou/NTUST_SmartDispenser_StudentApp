8a7cd7b1e3e26e5dafb4d5e5e5424afba90d2f94
Aldy <muhamadaldy17@gmail.com>
2019-07-15 16:06:12 +0800

    static-variable class ver 2019.07.15
    - add KEY__DEVICE_ID
    - remove device id key name for mt-progress and nearby-disp

--- src/app/classes/StaticVariable/static-variable.ts
+++ src/app/classes/StaticVariable/static-variable.ts
@@ -3,6 +3,7 @@ export class StaticVariable {
     public static KEY__LAST_DATE = "last_date";
     public static KEY__LAST_PAGE = "last_page";
     
+    public static KEY__DEVICE_ID = "device_id";
     public static KEY__NEARBY_DISPENSER__DEVICE_ID = "nD_deviceId";
     public static KEY__MAINTENANCE_PROGRESS__DEVICE_ID = "mP_deviceId";
 
